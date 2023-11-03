ba6f219bd48de180e9529d3a0a9345686b05e3d1
Denis Niwemugisha <kidemana@gmail.com>
2023-11-03 10:00:47 +0200

    clean the codebase

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const submitButton = document.querySelector(".primary-btn");
-document.addEventListener('DOMContentLoaded', (e) => {
-    console.log("submit button", submitButton);
-});
